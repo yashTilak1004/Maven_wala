62bc307c876e4f943d3d20f3aa86b5b8889c424b
yashTilak1004 <yashwanthtilak@gmail.com>
2023-12-06 11:49:01 +0530

    First

---
+++ src/main/java/in/kmit/Hospital_Management/App.java
@@ -0,0 +1,13 @@
+package in.kmit.Hospital_Management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.print("Hello");
+    }
+}
---
+++ src/test/java/in/kmit/Hospital_Management/AppTest.java
@@ -0,0 +1,20 @@
+package in.kmit.Hospital_Management;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
